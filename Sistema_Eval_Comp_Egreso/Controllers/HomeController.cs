9560ad0f6a22e88324cf4bbb10a36fd63eb29a8c
DssX <DssX@DESKTOP-0AICQB3>
2023-05-15 19:20:55 -0700

    Agregar archivos de proyecto.

---
+++ Sistema_Eval_Comp_Egreso/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistema_Eval_Comp_Egreso.Models;
+using System.Diagnostics;
+
+namespace Sistema_Eval_Comp_Egreso.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult EvalCompetencias()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public JsonResult GetTime()
+        {
+            DateTime time = DateTime.Now;
+            string response = "{\"time\": \"" + time + "\"}";
+            return Json(response);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Sistema_Eval_Comp_Egreso/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_Eval_Comp_Egreso.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
